cb89324872eb5bc5227a7d50d29fb14e05eb4e75
Valentin Besse <valentinbesse@aumbox.net>
2018-04-16 14:59:01 +0200

    Choix nombre et mise fonctionnelles.

--- main.py
+++ main.py
@@ -20,22 +20,76 @@ argent = 1000	# Somme initiale du joueur
 continuerPartie = True	# Booleen de la boucle de jeu
 
 print("""Bienvenu(e) au Casino de Valo ! Vous avez""",argent,"""€""")
-print("""Vous vous installez à une roulette.""")
+print("""Vous vous installez à une roulette.\n""")
 
 print("""Souhaitez vous connaître les règles du jeux ? o/n""")	# Test si le joueur veut connaitre les règles
 testRegle = input()
 
+
 if testRegle == "O" or testRegle == "o":	# Boucle de lecture des règles
+
 	print("""Les règles du jeu sont :""")
-	print("""- Vous misez sur un numéro compris entre 0 et 49 (50 numéros en tout). En choisissant le numéro, vous déposer la somme que vous souhaitez.""")
+	print("""- Vous misez sur un numéro compris entre 1 et 50 (50 numéros en tout). En choisissant le numéro, vous déposer la somme que vous souhaitez.""")
 	print("""- Le croupier lance la roulette, lâche la bille et quand la roulette s'arrête, relève le numéro de la case dans laquelle la bille s'est arrêtée. C'est le numéro gagnant.""")
 	print("""- Les numéros pairs sont de couleur noire, les numéros impairs sont de couleur rouge.""")
 	print("""- Si le numéro gagnant est celui que vous aviez choisi, vous gagnez 3 fois la somme misée.""")
 	print("""- Si le numéro gagnant est de la même couleur que le numéro que vous aviez choisi, vous gagnez 50% de la somme misé.""")
 	print("""- Sinon vous perdre votre mise.""")
 	
+print("""\n""")	
 	
 while continuerPartie:	
+	
+	numeroMise = -1
+	
+	while numeroMise < 0 or numeroMise > 49:
+	
+		print("""Sur quel nombre souhaitez vous miser ? 1-50""")
+		numeroMise = input()
+		# print("""\n""")
+	
+		try:
+			numeroMise = int(numeroMise)
+		except ValueError:
+			print("""Vous n'avez pas saisi de nombre.""")
+			numeroMise = -1
+			continue
+			
+		numeroMise = numeroMise - 1
+	
+		if numeroMise < 0:
+		
+			print("""Vous avez saisi un nombre inférieur à 1.""")
+		
+		if numeroMise > 49:
+			
+			print("""Vous avez saisi un nombre supérieur à 50.""")
+		
+	print("""Vous choississez le nombre""",numeroMise + 1,""".\n""")
+	
+	
+	argentMise = -1
+		
+	while argentMise <= 0 or argentMise > argent:
+		
+		print("""Combien souhaitez vous miser ?""")
+		argentMise = input()
+		# print("""\n""")
+		
+		try:
+			argentMise = int(argentMise)
+		except ValueError:
+			print("""Vous n'avez pas saisi de nombre.""")
+			argentMise = -1
+			continue
+	
+		if argentMise > argent:
+			print("""Vous ne pouvez miser plus que la totalité de votre argent.""")
+		
+		if argentMise <= 0:
+			print("""Vous ne pouvez pas miser une somme nulle ou négative.""")
+	
+	print("""Vous misez""",argentMise,"""€.\n""")
 	
 	print("""Voulez vous continuez ? o/n""")	# Test si joueur souhaite ou non continuer à jouer
 	testContinuerPartie = input()
